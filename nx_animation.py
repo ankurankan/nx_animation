b659f1edff77b2b948beff1cfbdbe0aca5a1462c
Ankur Ankan <ankurankan@gmail.com>
2013-10-26 09:55:34 +0530

    add set_node_color function

--- nx_animation.py
+++ nx_animation.py
@@ -44,7 +44,8 @@ def set_node_position(G, **kwargs):
         print("position argument required")
         sys.exit(0)
 
-    fig = plt.gcf()
+    # TODO: Recursive call the below for each node for it to work when no node is specified
+
     axes = plt.gca()
     node_index = G.nodes().index(node)
     no_of_nodes = G.number_of_nodes()
@@ -72,4 +73,72 @@ def set_node_position(G, **kwargs):
     plt.draw()
 
 def set_node_color(G, **kwargs):
-    pass
+    """
+    Changes node colors
+
+    Parameters
+    ----------
+    G     : Graph whose's node's color needs to be changed
+
+    kwargs
+    ------
+    node  : The node whose color needs to be changed. If node not specified
+            and only one color is specified changes all the nodes to that
+            color. If node not specified and color is a list or tuple of
+            colors, each node will be changed to its corresponding color.
+
+    color : Can be a single value of a list of colors.kwargs
+
+    The following color abbreviations are supported:
+    ==========  ========
+    character   color
+    ==========  ========
+    'b'         blue
+    'g'         green
+    'r'         red
+    'c'         cyan
+    'm'         magenta
+    'y'         yellow
+    'k'         black
+    'w'         white
+    ==========  ========
+
+    Examples:
+    set_node_color(G,
+    """
+    try:
+        node = kwargs['node']
+    except KeyError:
+        node = None
+    try:
+        color = kwargs['position']
+    except KeyError:
+        print("color argument required")
+        sys.exit(0)
+
+    colors_dict = {'b': [0.,    0.,    1.,  1.],
+                   'g': [0.,    0.5,   0.,  1.],
+                   'r': [1.,    0.,    0.,  1.],
+                   'c': [0.,    0.75,  0.75,  1.],
+                   'm': [0.75,  0.,    0.75,  1.],
+                   'y': [0.75,  0.75,  0.,  1.],
+                   'k': [0.,    0.,    0.,  1.],
+                   'w': [1.,    1.,    1.,  1.]}
+
+    fig = plt.gcf()
+    axes = plt.gca()
+    no_of_nodes = G.number_of_nodes()
+    nodes_collection = axes.get_children()[no_of_nodes + 2]
+
+    # if node is specified manually changing the facecolor and edgecolor array so that
+    # the colors for other nodes are retained
+    if node:
+        node_index = G.nodes().index(node)
+        facecolor_array = nodes_collection.get_facecolor()
+        edgecolor_array = nodes_collection.get_edgecolor()
+        facecolor_array[node_index] = colors_dict[color]
+        edgecolor_array[node_index] = colors_dict[color]
+
+    # if node not specified call the matplotlibs set_color function
+    else:
+        nodes_collection.set_color(color)
